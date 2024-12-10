37488992ddfbab7d026840f7e0e647e35e46edd1
Jakub Rudnicki <hyperek@vp.pl>
2024-12-10 09:52:20 +0100

    [Improvement] Better BackupManager
    New version should create backups using recursive to create a
    copy of all files and folders under path. The backup is then saved as zip file.

--- Code/src/main/java/com/github/happyuky7/separeworlditems/utils/BackupManager.java
+++ Code/src/main/java/com/github/happyuky7/separeworlditems/utils/BackupManager.java
@@ -2,12 +2,12 @@ package com.github.happyuky7.separeworlditems.utils;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
+import java.io.*;
 import java.text.SimpleDateFormat;
 import java.util.*;
 import java.util.logging.Level;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 
 /**
  * Manages backups of plugin configuration files.
@@ -47,7 +47,7 @@ public class BackupManager {
      * based on the last modified time.
      */
     private void initializeBackupQueue() {
-        File[] backups = backupFolder.listFiles((dir, name) -> name.endsWith(".bak"));
+        File[] backups = backupFolder.listFiles((dir, name) -> name.endsWith(".zip"));
         if (backups != null) {
             Arrays.sort(backups, Comparator.comparingLong(File::lastModified));
             for (File backup : backups) {
@@ -66,21 +66,22 @@ public class BackupManager {
     }
 
     /**
-     * Creates a backup of the specified file.
+     * Creates a ZIP backup of the specified directory.
      *
-     * @param file the file to back up
+     * @param sourceFolder the folder to back up
      */
-    public void createBackup(File file) {
-        if (!file.exists()) {
-            plugin.getLogger().warning("File does not exist: " + file.getName());
+    public void createBackup(File sourceFolder) {
+        if (!sourceFolder.exists() || !sourceFolder.isDirectory()) {
+            plugin.getLogger().warning("Specified folder does not exist or is not a directory: " + sourceFolder.getName());
             return;
         }
 
         String timestamp = new SimpleDateFormat("yyyy-MM-dd_HH-mm-ss").format(new Date());
-        File backupFile = new File(backupFolder, file.getName() + "_" + timestamp + ".bak");
+        File backupFile = new File(backupFolder, sourceFolder.getName() + "_" + timestamp + ".zip");
 
-        try {
-            Files.copy(file.toPath(), backupFile.toPath());
+        try (FileOutputStream fos = new FileOutputStream(backupFile);
+             ZipOutputStream zos = new ZipOutputStream(fos)) {
+            zipDirectory(sourceFolder, sourceFolder.getName(), zos);
             plugin.getLogger().info("Backup created: " + backupFile.getName());
             backupQueue.add(backupFile);
 
@@ -93,17 +94,46 @@ public class BackupManager {
                 }
             }
         } catch (IOException e) {
-            plugin.getLogger().log(Level.SEVERE, "Failed to create backup for: " + file.getName(), e);
+            plugin.getLogger().log(Level.SEVERE, "Failed to create backup for: " + sourceFolder.getName(), e);
+        }
+    }
+
+    /**
+     * Recursively zips a directory.
+     *
+     * @param folder the folder to zip
+     * @param basePath the base path to preserve folder structure
+     * @param zos the ZIP output stream
+     * @throws IOException if an I/O error occurs
+     */
+    private void zipDirectory(File folder, String basePath, ZipOutputStream zos) throws IOException {
+        for (File file : Objects.requireNonNull(folder.listFiles())) {
+            String entryName = basePath + File.separator + file.getName();
+            if (file.isDirectory()) {
+                zipDirectory(file, entryName, zos);
+            } else {
+                try (FileInputStream fis = new FileInputStream(file)) {
+                    ZipEntry entry = new ZipEntry(entryName);
+                    zos.putNextEntry(entry);
+
+                    byte[] buffer = new byte[1024];
+                    int length;
+                    while ((length = fis.read(buffer)) > 0) {
+                        zos.write(buffer, 0, length);
+                    }
+                    zos.closeEntry();
+                }
+            }
         }
     }
 
     /**
      * Starts the automatic backup task.
      *
-     * @param file the file to back up
+     * @param sourceFolder the folder to back up
      */
-    public void startAutoBackup(File file) {
-        plugin.getServer().getScheduler().runTaskTimerAsynchronously(plugin, () -> createBackup(file), 0L,
+    public void startAutoBackup(File sourceFolder) {
+        plugin.getServer().getScheduler().runTaskTimerAsynchronously(plugin, () -> createBackup(sourceFolder), 0L,
                 backupInterval / 50);
     }
 }
